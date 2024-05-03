519678cd134d95c0bea2e9fd78e8f5e57886f118
XxImDavexX <david411dark@gmail.com>
2024-05-03 12:03:41 -0600

    Test connection with the API

--- src/TestChallengeCurrencyConvert.java
+++ src/TestChallengeCurrencyConvert.java
@@ -1,2 +1,8 @@
+import java.io.IOException;
+
 public class TestChallengeCurrencyConvert {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ConnectToExchangeRateApi connectToExchangeRateApi = new ConnectToExchangeRateApi();
+        System.out.println(connectToExchangeRateApi.getCurrencyValue("mxn"));
+    }
 }
