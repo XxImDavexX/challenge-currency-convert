27510fec1f31f3923cd2169932921d2100f45a78
XxImDavexX <david411dark@gmail.com>
2024-05-03 13:50:49 -0600

    Update TestChallengeCurrencyConvert

--- src/TestChallengeCurrencyConvert.java
+++ src/TestChallengeCurrencyConvert.java
@@ -1,8 +1,21 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class TestChallengeCurrencyConvert {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner keyboard = new Scanner(System.in);
+        double amount;
+        String baseCurrency;
+        String targetCurrency;
+        System.out.println("Insert your code currency : ");
+        baseCurrency = keyboard.next();
+        System.out.println("Enter the currency code you want to convert to : ");
+        targetCurrency = keyboard.next();
+        System.out.println("Insert the amount of " + baseCurrency + " : ");
+        amount = keyboard.nextDouble();
         ConnectToExchangeRateApi connectToExchangeRateApi = new ConnectToExchangeRateApi();
-        System.out.println(connectToExchangeRateApi.getCurrencyValue("mxn"));
+        Currencies currencies = new Currencies(connectToExchangeRateApi.getCurrencyValue(baseCurrency, targetCurrency, amount));
+        currencies.setAmount(amount);
+        System.out.println(currencies);
     }
 }
