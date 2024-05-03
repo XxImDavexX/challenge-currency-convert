26c0efb904b0b7d4e0953145b15f8ed57704d28f
XxImDavexX <david411dark@gmail.com>
2024-05-03 13:50:20 -0600

    Update class ConnectToExchangeRateApi, connect this class with the record DataCurrencies

--- src/ConnectToExchangeRateApi.java
+++ src/ConnectToExchangeRateApi.java
@@ -8,15 +8,16 @@ import java.net.http.HttpResponse;
 
 public class ConnectToExchangeRateApi {
 
-    String getCurrencyValue (String currency) throws IOException, InterruptedException {
-        currency = currency.toUpperCase();
+    DataCurrencies getCurrencyValue (String baseCurrency, String targetCurrency, double amount) throws IOException, InterruptedException {
+        baseCurrency = baseCurrency.toUpperCase();
+        targetCurrency = targetCurrency.toUpperCase();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://v6.exchangerate-api.com/v6/d4ac0b3e92e54d2031666139/latest/" + currency ))
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/d4ac0b3e92e54d2031666139/pair/" + baseCurrency + "/" + targetCurrency + "/" + amount ))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
+        return new Gson().fromJson( response.body(), DataCurrencies.class);
 
     }
 }
