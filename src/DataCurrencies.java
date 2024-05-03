696da8dacbfccabba08ba5b4352f81395022cc88
XxImDavexX <david411dark@gmail.com>
2024-05-03 13:49:09 -0600

    Create Record DataCurrencies

---
+++ src/DataCurrencies.java
@@ -0,0 +1,5 @@
+public record DataCurrencies(String base_code,
+                             String target_code,
+                             double conversion_rate,
+                             double conversion_result) {
+}
